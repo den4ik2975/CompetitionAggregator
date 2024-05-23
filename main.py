165536123757acd7b395009b1ec00d0e9bef62b7
den4ik2975 <98264297+den4ik2975@users.noreply.github.com>
2024-05-23 20:29:14 +0300

    Changed main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 from src.setup import setup_fastapi, setup_logging
 
 if __name__ == "__main__":
-    app_fastapi = setup_fastapi()
+    app_fastapi = setup_fastapi()  # setup
 
 
     @app_fastapi.on_event("startup")
